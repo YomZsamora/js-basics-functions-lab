ac3c789523909a3c0960d4a5a74f616d795cc87c
yomZsamora <samora.y@adzumi.co.ke>
2022-01-13 21:34:00 +0300

    Returns a distance in feet.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const scuberHeadquarters = 42;
 
-// Returns a distance in blocks
+// Returns a distance in blocks and also calculates distances below 42nd street
 function distanceFromHqInBlocks(pickUpLocation) {
     const distanceInBlocks = pickUpLocation > scuberHeadquarters ? pickUpLocation - scuberHeadquarters : scuberHeadquarters - pickUpLocation;
     return distanceInBlocks;
+}
+
+// Returns a distance in feet
+function distanceFromHqInFeet(pickUpLocation) {
+    const distanceInFeet = pickUpLocation > scuberHeadquarters ? (pickUpLocation - scuberHeadquarters)*264 : (scuberHeadquarters - pickUpLocation)*264;
+    return distanceInFeet;
 }
